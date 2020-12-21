5c581ba1a0504038bab1fa1a50dea2fecd1a3b15
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-21 01:11:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,20 @@
+<<<<<<< HEAD
 def starts_with_a_vowel?(word)
   if (word.match(/\A[aeoiuAEIOU]/)==nil)
     return false 
   else 
+=======
+def starts_with_a_vowel?(word){
+  if (word.match(/^[aeoiuAEIOU]/i)) == nil)
+    return false
+  else
+>>>>>>> 65f788a5a8f236be9ad2c34c8446ce6729fbdd5c
     return true
   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+<<<<<<< HEAD
   text.scan(/un\w*ing/)
 end
 
@@ -30,4 +38,19 @@ def valid_phone_number?(phone)
     return true
     
   end
+=======
+  #text.scan(/\A(un)\w*ing/))
+end
+
+def words_five_letters_long(text)
+  #text.scan(/^\w{5}/)
+end
+
+def first_word_capitalized_and_ends_with_punctuation?(text)
+
+end
+
+def valid_phone_number?(phone)
+
+>>>>>>> 65f788a5a8f236be9ad2c34c8446ce6729fbdd5c
 end
